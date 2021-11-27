b73d8e8cc82539e2df4e0b3f66d1dbae9ad9aa41
Sean Carrington <seanzi@msn.com>
2021-11-27 19:56:43 +0000

    first commit

---
+++ app/routes/jokes.tsx
@@ -0,0 +1,23 @@
+import type { LinksFunction } from "remix";
+import { Outlet } from "remix";
+import stylesUrl from "../styles/jokes.css";
+
+export let links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl,
+    },
+  ];
+};
+
+export default function JokesRoute() {
+  return (
+    <div>
+      <h1>J🤪KES</h1>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
